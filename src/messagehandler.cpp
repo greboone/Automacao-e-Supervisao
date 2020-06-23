9532b524fc643a08255ea3212e941e6f8cb6f132
Greboone <guto.ebone3553@gmail.com>
2020-06-23 10:23:37 -0300

    Inserido troca de baudrate

--- src/messagehandler.cpp
+++ src/messagehandler.cpp
@@ -1,10 +1,11 @@
 #include "messagehandler.h"
 
+unsigned int BAUDRATE ;//= 9600;
 
 void msgInit()
 {
-    Serial.begin(9600);
-    Serial.println("Serial Inciado.");
+    Serial.begin(BAUDRATE);
+    Serial.println("Serial Iniciado.");
     iniciaRtc();
 }
 
@@ -160,6 +161,11 @@ if (Serial.available() > 0)
               break;
             }
 
+            case '3':
+            {
+              setBaud(msg);
+              break;
+            }
             default:
               break;
 
@@ -190,144 +196,23 @@ void schedule(){
   retorna();
 }
 
-/*
-// Funcoes para o Switch case
-void read_coil(String msg){
-  int coil = ((msg[5]-'0')*10 + (msg[6]-'0')) + OUTPUT_OFFSET;
-  // debug
-  Serial.print("Leitura na Saida ");
-  Serial.println(coil-OUTPUT_OFFSET);
+void setBaud(String msg){
+  int novo = 0;
+  novo = ((msg[4] - '0') * 100000) + novo;
+  novo = ((msg[5] - '0') * 10000) + novo;
+  novo = ((msg[6] - '0') * 1000) + novo;
+  novo = ((msg[7] - '0') * 100) + novo;
+  novo = ((msg[8] - '0') * 10) + novo;
+  novo = ((msg[9] - '0')) + novo;
+  if(novo == 300 || novo == 1200 || novo == 2400 || novo == 4800 || novo == 9600 || 
+     novo == 19200 || novo == 38400 || novo == 57600 || novo == 74880 || novo == 115200 ||
+     novo == 230400 || novo == 250000 || novo == 500000)
+  {
+    BAUDRATE = novo;
+  }else{
+    Serial.print("Valor invalido!");
+  }
 
-  // Ligada ou desligada?
-  if(!digitalRead(coil))
-  {
-      msg[7]='F';
-      msg[8]='F';
-      msg[9]='0';
-      msg[10]='0';
-  }
-  else
-  {
-    msg[7]='0';
-    msg[8]='0';
-    msg[9]='0';
-    msg[10]='0';
-  }
-  
-  // Resposta com o valor atual da entrada..
-  Serial.print("Resposta do Escravo: ");
-  Serial.println(msg);
+  Serial.println("Alterando BaudRate");
+  msgInit();
 }
-
-void read_input(String msg){
-  // numero da entrada
-  int contact = ((msg[5]-'0')*10 + (msg[6]-'0')) + INPUT_OFFSET;
-
-  // debug
-  Serial.print("Leitura na Entrada ");
-  Serial.println(contact-INPUT_OFFSET);
-
-  // Ligada ou desligada?
-  if(digitalRead(contact))
-  {
-      msg[7]='F';
-      msg[8]='F';
-      msg[9]='0';
-      msg[10]='0';
-  }
-  else
-  {
-    msg[7]='0';
-    msg[8]='0';
-    msg[9]='0';
-    msg[10]='0';
-  }
-  
-  // Resposta com o valor atual da entrada..
-  Serial.print("Resposta do Escravo: ");
-  Serial.println(msg);
-}
-
-void write_coil(String msg){
-  // string para inteiro aplicando offset, saida 1 no Arduino está mapeada para o pino 8, saída 2 para o pino 9 ....
-  int coil = ((msg[5]-'0')*10 + (msg[6]-'0')) + OUTPUT_OFFSET;
-
-  // padrao é desligar
-  int value = OFF;
-
-  // debug
-  Serial.print("Escrita na Saida ");
-  Serial.println(coil-OUTPUT_OFFSET);
-
-  // ligar ou desligar?
-  if(msg[7]=='F' && msg[8]=='F' && msg[9]=='0' && msg[10]=='0')
-  {
-    value = ON;
-    Serial.println(" -> Mudar para Ligado");
-  }
-  else if(msg[7]=='0' && msg[8]=='0' && msg[9]=='0' && msg[10]=='0')
-  {
-    value = OFF;
-    Serial.println(" Mudar para Desligado");
-  }
-  // executao comando
-  digitalWrite(coil, value);
-  
-  // Para esse caso, a resposta é um simples echo da mensagem original
-  Serial.print("Resposta do Escravo: ");
-  Serial.println(msg);
-}
-
-void read_analog(String msg){
-  // decodifica entrada a ser lida
-  int ain = ((msg[5]-'0')*10 + (msg[6]-'0')) + ANALOG_INPUT_OFFSET;
-
-  // debug
-  Serial.print("Leitura na Entrada Analogica ");
-  Serial.println(ain+1);
-
-  // executao comando
-  uint16_t value = analogRead(ain);
-  
-  // int para string
-  char buf[5];
-  sprintf(buf,"%04d", value);
-
-  // monta valor de retonro
-  msg[7]=buf[0];
-  msg[8]=buf[1];
-  msg[9]=buf[2];
-  msg[10]=buf[3];
-  
-  // Responde para o mestre
-  Serial.print("Resposta do Escravo: ");
-  Serial.println(msg);
-}
-
-void write_analog(String msg){
-  // decodifica entrada a ser lida
-  int aon = ((msg[5]-'0')*10 + (msg[6]-'0')) + ANALOG_OUTPUT_OFFSET;
-  // de string para inteito
-  int value = (msg[7]-'0')*1000 + (msg[8]-'0')*100 + (msg[9]-'0')*10 +(msg[10]-'0');
-
-  // debug
-  Serial.print("Escrita na Saida Analogica ");
-  Serial.println(aon-ANALOG_OUTPUT_OFFSET);
-  Serial.print("Valor: ");
-  Serial.println(value);
-
-  // escreve na saída
-  analogWrite(aon, value);
-
-  // Para esse caso, a resposta é um simples echo da mensagem original
-  Serial.print("Resposta do Escravo: ");
-  Serial.println(msg);
-}
-*/
-
-
-
-
-
-
-
